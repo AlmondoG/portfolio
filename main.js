00d092218000b4544958257c755ff6920f86d3a1
Amin Ghanimati <AminG3M@yahoo.com>
2018-08-22 17:25:06 -0700

    Encrypted sensitive info

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(document).ready(initializeApp);
 
 function initializeApp() {
+    contactInfoE();
+    contactInfoPN();
     $(window).scroll(navBarClose);
 }
 
@@ -12,4 +14,16 @@ function navBarClose() {
             $(".navbar-collapse").collapse("hide");
         }
     }
+}
+
+function contactInfoE() {
+    var parts = ["AminG3M", "com", "@", ".", "yahoo"];
+    var email = parts[0] + parts[2] + parts[4] + parts[3] + parts[1];
+    $(".el-emailo").text(email);
+}
+
+function contactInfoPN() {
+    var parts = ["66", "045", "3", "858", "9"];
+    var number = parts[3] + parts[0] + parts[2] + parts[1] + parts[4];
+    $(".el-telephono").text(number);
 }
